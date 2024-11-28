7a3c3ac8e218a291f5e59b9784cdaec24ca0bf9c
sangil1018 <sangil1018@gmail.com>
2024-11-28 11:02:51 +0900

    1128 1100 1-4 완료

--- Assets/Gintotal/Scripts/TimeLineControl.cs
+++ Assets/Gintotal/Scripts/TimeLineControl.cs
@@ -6,7 +6,9 @@ public class TimeLineControl : MonoBehaviour
 {
     [SerializeField] private InputActionProperty forwardTrigger; // 오른쪽 컨트롤러 트리거
     [SerializeField] private InputActionProperty backwardTrigger;  // 왼쪽 컨트롤러 트리거
-    [SerializeField] private bool customPlay = false;
+    [SerializeField] private bool customPlay;
+    [SerializeField] private GameObject hands;
+    
     private PlayableDirector _timeline; // 타임라인 연결
     private const float PlaySpeed = 1f;
 
@@ -14,6 +16,7 @@ public class TimeLineControl : MonoBehaviour
 
     private void OnEnable()
     {
+        hands.SetActive(false);
         _timeline = GetComponent<PlayableDirector>();
         _timeline.Play();
         _timeline.Pause();
@@ -27,6 +30,7 @@ public class TimeLineControl : MonoBehaviour
             {
                 if (_timeline != null && _timeline.playableAsset != null)
                 {
+                    if (hands != null) hands.SetActive(true);
                     _timeline.Play();
                     return;
                 }
@@ -45,19 +49,26 @@ public class TimeLineControl : MonoBehaviour
             _isReversing = false;
             _timeline.playableGraph.GetRootPlayable(0).SetSpeed(PlaySpeed);
             if (_timeline.state != PlayState.Playing)
+            {
+                if (hands != null) hands.SetActive(true);
                 _timeline.Play();
+            }
         }
         // 왼쪽 트리거를 누르고 있을 때 역방향 재생
         else if (backwardTrigger.action.IsPressed())
         {
             _isReversing = true;
             if (_timeline.state == PlayState.Playing)
-                _timeline.Pause(); // 역방향 재생에서는 Play 중단
+            {
+                if (hands != null) hands.SetActive(true);
+                _timeline.Pause();
+            } // 역방향 재생에서는 Play 중단
         }
         // 트리거를 놓았을 때 멈춤
         else
         {
             _isReversing = false;
+            if (hands != null) hands.SetActive(false);
             _timeline.Pause();
         }
 
